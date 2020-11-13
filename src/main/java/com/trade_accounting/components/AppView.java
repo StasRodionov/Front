9c70132aa57fa4af56afb6efd8c0c9f0ffc920e2
Максим <sidor8maximus230@gmail.com>
2020-11-13 14:50:46 +0300

    Initial commit

---
+++ src/main/java/com/trade_accounting/components/AppView.java
@@ -0,0 +1,12 @@
+package com.trade_accounting.components;
+
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.router.Route;
+
+@Route
+public class AppView extends AppLayout {
+
+    public AppView() {
+
+    }
+}
