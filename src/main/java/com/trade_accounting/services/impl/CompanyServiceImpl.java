587960e91b9c5f0261f3e4f1bd31ae51fa5d85d3
Denis Pavlov <pavler495@gmail.com>
2020-11-22 01:58:35 +0300

    CompanyServiceImpl methods were wrapped with try/catch

--- src/main/java/com/trade_accounting/services/impl/CompanyServiceImpl.java
+++ src/main/java/com/trade_accounting/services/impl/CompanyServiceImpl.java
@@ -9,7 +9,6 @@ import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import retrofit2.Call;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 
