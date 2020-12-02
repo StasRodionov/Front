4be4dd837d64121a42d649c96bc9653117a7831e
DaniilNov <xa86@mail.ru>
2020-12-02 14:54:49 +0300

    Добавил WarehouseDto

---
+++ src/main/java/com/trade_accounting/models/dto/WarehouseDTO.java
@@ -0,0 +1,23 @@
+package com.trade_accounting.models.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class WarehouseDTO {
+    private Long id;
+
+    private String name;
+
+    private String sortNumber;
+
+    private String address;
+
+    private String commentToAddress;
+
+    private String comment;
+
+}
