667099e497e8548ff06d756baac7fdc40a62d9d5
troshina22 <d.troshina222@gmail.com>
2020-12-03 20:38:14 +0300

    add DepartmentDto

---
+++ src/main/java/com/trade_accounting/models/dto/DepartmentDto.java
@@ -0,0 +1,22 @@
+package com.trade_accounting.models.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DepartmentDto {
+
+    private Long id;
+
+    private String name;
+
+    private String sortNumber;
+
+    public DepartmentDto(String name, String sortNumber){
+        this.name = name;
+        this.sortNumber = sortNumber;
+    }
+}
