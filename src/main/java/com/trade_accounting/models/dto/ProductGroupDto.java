2ab9d70f31e4921d6de9942d2b4f8ff922c3d0f9
ElizabethGlazneva <valeragl999@gmail.com>
2020-12-03 13:10:10 +0300

    add DTO for ProductGroup

---
+++ src/main/java/com/trade_accounting/models/dto/ProductGroupDto.java
@@ -0,0 +1,19 @@
+package com.trade_accounting.models.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductGroupDto {
+
+    private Long id;
+
+    private String name;
+
+    private String sortNumber;
+
+    private Long parentId;
+}
