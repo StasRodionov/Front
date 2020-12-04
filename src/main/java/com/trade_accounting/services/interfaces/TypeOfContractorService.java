4d07060b8f5a7903cb03c67a8b7499aa2bc1564f
Maslak <andrey.maslak@softwareone.com>
2020-12-04 18:59:51 +0500

    add TypeOfContractorService v2

--- src/main/java/com/trade_accounting/services/interfaces/TypeOfContractorService.java
+++ src/main/java/com/trade_accounting/services/interfaces/TypeOfContractorService.java
@@ -7,6 +7,7 @@ import java.util.List;
 public interface TypeOfContractorService {
 
     List<TypeOfContractorDto> getAll();
+
     TypeOfContractorDto getById(Long id);
 
     void create(TypeOfContractorDto typeOfContractorDto);
