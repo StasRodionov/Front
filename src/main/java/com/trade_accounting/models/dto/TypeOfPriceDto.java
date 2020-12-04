6213f5dfa177a016aa6b3d571adcdeac413d61d6
Anna <maksimova2800@gmail.com>
2020-12-04 15:43:20 +0300

    Add ContractorDto

--- src/main/java/com/trade_accounting/models/dto/TypeOfPriceDto.java
+++ src/main/java/com/trade_accounting/models/dto/TypeOfPriceDto.java
@@ -3,10 +3,8 @@ package com.trade_accounting.models.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Data
-@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class TypeOfPriceDto {
