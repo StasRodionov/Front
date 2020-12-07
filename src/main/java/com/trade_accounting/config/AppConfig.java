661b45643dcbb3839ed023fc39f57cd84020abf2
Максим <sidor8maximus230@gmail.com>
2020-12-07 09:48:21 +0300

    refactor

--- src/main/java/com/trade_accounting/config/AppConfig.java
+++ src/main/java/com/trade_accounting/config/AppConfig.java
@@ -1,4 +1,4 @@
-package com.trade_accounting;
+package com.trade_accounting.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
