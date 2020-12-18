8fae90835a9a0de728ecb2f4abb79a8c3aef17d9
Anna <maksimova2800@gmail.com>
2020-12-18 12:32:43 +0300

    Added TypeOfContractorServiceImpl and TypeOfContractorApi

--- src/main/java/com/trade_accounting/services/impl/TypeOfContractorServiceImpl.java
+++ src/main/java/com/trade_accounting/services/impl/TypeOfContractorServiceImpl.java
@@ -23,7 +23,7 @@ public class TypeOfContractorServiceImpl implements TypeOfContractorService {
     private TypeOfContractorDto typeOfContractorDto;
 
     @Autowired
-    public TypeOfContractorServiceImpl(@Value("${typeofcontractor_url}") String typeOfContractorUrl, Retrofit retrofit) {
+    public TypeOfContractorServiceImpl(@Value("${type_of_contractor_url}") String typeOfContractorUrl, Retrofit retrofit) {
 
         this.typeOfContractorUrl = typeOfContractorUrl;
         typeOfContractorApi = retrofit.create(TypeOfContractorApi.class);
