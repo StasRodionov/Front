b33200fbcfdf33f9c1474aa21f473f8481f17681
Kostya Petrov <KostyaPetrov1986@gmail.com>
2020-12-29 13:47:04 +0400

    add ContractroGroupService, ContractorGroupApi, ContractorGroupServiceImpl

---
+++ src/main/java/com/trade_accounting/services/impl/ContractServiceImpl.java
@@ -0,0 +1,209 @@
+package com.trade_accounting.services.impl;
+
+import com.trade_accounting.models.dto.BankAccountDto;
+import com.trade_accounting.models.dto.CompanyDto;
+import com.trade_accounting.models.dto.ContractDto;
+import com.trade_accounting.models.dto.ContractorDto;
+import com.trade_accounting.models.dto.ContractorGroupDto;
+import com.trade_accounting.models.dto.LegalDetailDto;
+import com.trade_accounting.models.dto.TypeOfContractorDto;
+import com.trade_accounting.models.dto.TypeOfPriceDto;
+import com.trade_accounting.services.interfaces.ContractService;
+import com.trade_accounting.services.interfaces.api.ContractApi;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+import javax.annotation.PostConstruct;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+@Service
+@Slf4j
+public class ContractServiceImpl implements ContractService {
+    private final ContractApi contractApi;
+
+    private final String contractUrl;
+
+    private List<ContractDto> contractDtoList;
+
+    private ContractDto contractDto;
+
+    @Autowired
+    public ContractServiceImpl(@Value("${contract_url}") String contractUrl, Retrofit retrofit) {
+        contractApi = retrofit.create(ContractApi.class);
+        this.contractUrl = contractUrl;
+    }
+
+    @Override
+    public List<ContractDto> getAll() {
+        Call<List<ContractDto>> contractDtoListCall = contractApi.getAll(contractUrl);
+
+        contractDtoListCall.enqueue(new Callback<>() {
+            @Override
+            public void onResponse(Call<List<ContractDto>> call, Response<List<ContractDto>> response) {
+                if (response.isSuccessful()) {
+                    contractDtoList = response.body();
+                    Objects.requireNonNull(contractDtoList).forEach(contr -> contr.setDate(contr.getContractDate()));
+                    Objects.requireNonNull(contractDtoList).forEach(contr -> contr.getLegalDetailDto().
+                            setDate(contr.getLegalDetailDto().getDateOfTheCertificate()));
+                    log.info("Успешно выполнен запрос на получение списка ContractDto");
+                } else {
+                    log.error("Произошла ошибка при отправке запроса на получение списка ContractDto: {}", response.errorBody());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<ContractDto>> call, Throwable throwable) {
+                log.error("Произошла ошибка при отправке запроса на получение списка ContractDto: ", throwable);
+            }
+        });
+
+        return contractDtoList;
+    }
+
+    @Override
+    public ContractDto getById(Long id) {
+
+        Call<ContractDto> contractDtoCall = contractApi.getById(contractUrl, id);
+        contractDtoCall.enqueue(new Callback<>() {
+            @Override
+            public void onResponse(Call<ContractDto> call, Response<ContractDto> response) {
+                if (response.isSuccessful()) {
+                    contractDto = response.body();
+                    Objects.requireNonNull(contractDto).setDate(contractDto.getContractDate());
+                    Objects.requireNonNull(contractDto).getLegalDetailDto().
+                            setDate(contractDto.getLegalDetailDto().getDateOfTheCertificate());
+
+                    log.info("Успешно выполнен запрос на получение экземпляра ContractDto с id = {}", id);
+                } else {
+                    log.error("Произошла ошибка при отправке запроса на получение ContractDto с id = {}: {}",
+                            id, response.errorBody());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ContractDto> call, Throwable throwable) {
+                log.error("Произошла ошибка при отправке запроса на получение ContractDto c id = {}: {}",
+                        id, throwable);
+            }
+        });
+        return contractDto;
+    }
+
+    @Override
+    public void create(ContractDto contractDto) {
+        Call<Void> contractDtoCall = contractApi.create(contractUrl, contractDto);
+
+        contractDtoCall.enqueue(new Callback<>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()) {
+                    log.info("Успешно выполнен запрос на создание нового экземпляра {}", contractDto);
+                } else {
+                    log.error("Произошла ошибка при отправке запроса на создание нового экземпляра {}: {}",
+                            contractDto, response.errorBody());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable throwable) {
+                log.error("Произошла ошибка при отправке запроса на создание нового экземпляра {}: {}",
+                        contractDto, throwable);
+            }
+        });
+    }
+
+    @Override
+    public void update(ContractDto contractDto) {
+        Call<Void> contractDtoCall = contractApi.update(contractUrl, contractDto);
+
+        contractDtoCall.enqueue(new Callback<>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()) {
+                    log.info("Успешно выполнен запрос на обновление {}", contractDto);
+                } else {
+                    log.error("Произошла ошибка при отправке запроса на обновление {}: {}",
+                            contractDto, response.errorBody());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable throwable) {
+                log.error("Произошла ошибка при отправке запроса на обновление {}: {}",
+                        contractDto, throwable);
+            }
+        });
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        Call<Void> contractDtoCall = contractApi.deleteById(contractUrl, id);
+
+        contractDtoCall.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()) {
+                    log.info("Успешно выполнен запрос на удаление ContractDto c id = {}", id);
+                } else {
+                    log.error("Произошла ошибка при отправке запроса на удаление ContractDto c id = {}: {}",
+                            id, response.errorBody());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable throwable) {
+                log.error("Произошла ошибка при отправке запроса на удаление ContractDto c id = {}: {}",
+                        id, throwable);
+            }
+        });
+    }
+
+
+
+    @PostConstruct
+    public void Test() {
+
+
+        TypeOfContractorDto typeOfContractorDto = new TypeOfContractorDto(1L, "name", "1");
+        LegalDetailDto legalDetailDto = new LegalDetailDto(1L, "lastName",
+                "firstName", "middleName", "address", "commentToAddress", "0000000000",
+                "000", "000", "000", LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")), LocalDate.now(), typeOfContractorDto);
+
+        CompanyDto company= new CompanyDto(2L, "name", "0000000000",
+                "11", "000000", "000000", "email",
+                true, "address", "commentToAddress", "leader",
+                "leaderManagerPosition", "leaderSignature", "chiefAccountant",
+                "chiefAccountantSignature", "stamp", legalDetailDto);
+
+        BankAccountDto bank = new BankAccountDto(1L, "rcbic", "bank", "address",
+                "correspondentAccount", "account", true, "1");
+
+        ContractorGroupDto contractorGroupDto = new ContractorGroupDto(1L, "name", "1");
+
+        TypeOfPriceDto typeOfPriceDto = new TypeOfPriceDto(1L, "name", "1");
+
+        ContractorDto contractor = new ContractorDto(1L, "name", "0000000000", "sortNumber",
+                "000000", "000000", "email", "address", "commentToAddress",
+                "comment", contractorGroupDto, typeOfContractorDto, typeOfPriceDto, Collections.singletonList(bank), legalDetailDto);
+
+
+
+        create(new ContractDto(2L, "1", LocalDate.now(), company, bank, contractor,
+                BigDecimal.valueOf(33333.0), true, "jtgnkjrtgnkrgjn ", legalDetailDto));
+
+        getById(1L);
+//        getAll();
+//        deleteById(2L);
+    }
+}
