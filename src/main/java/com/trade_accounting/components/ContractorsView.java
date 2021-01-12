35f2a37f4f077b5f7a508ce68e445ac33abeb5ae
frolowyan <frolowyan@gmail.com>
2021-01-12 16:29:47 +0300

    add contract view

--- src/main/java/com/trade_accounting/components/ContractorsView.java
+++ src/main/java/com/trade_accounting/components/ContractorsView.java
@@ -1,7 +1,9 @@
 package com.trade_accounting.components;
 
+import com.trade_accounting.services.interfaces.ContractService;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.PageTitle;
@@ -9,16 +11,31 @@ import com.vaadin.flow.router.Route;
 
 @Route(value = "contractors", layout = AppView.class)
 @PageTitle("Контрагенты")
-public class ContractorsView extends Div{
+public class ContractorsView extends Div {
 
-    public ContractorsView() {
-        add(configurationSubMenu());
+    private final Div div;
+
+    private final ContractService contractService;
+
+    public ContractorsView(ContractService contractService) {
+        this.contractService = contractService;
+        div = new Div();
+
+        add(configurationSubMenu(), div);
     }
 
     private Tabs configurationSubMenu() {
+
+        HorizontalLayout contractors = new HorizontalLayout(new Label("Контрагенты"));
+        HorizontalLayout contracts = new HorizontalLayout(new Label("Договоры"));
+        contracts.addClickListener(e -> contracts.getUI().ifPresent(ui -> {
+            div.removeAll();
+            div.add(new ContractsView(contractService));
+        }));
+
         Tabs tabs = new Tabs(
-                new Tab(new Label("Контрагенты")),
-                new Tab(new Label("Договоры"))
+                new Tab(contractors),
+                new Tab(contracts)
         );
         tabs.setOrientation(Tabs.Orientation.HORIZONTAL);
         return tabs;
