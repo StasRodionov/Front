9e52b14b5d78d370163ddd5f04ca727c24ffe964
Keyneto <Brize2302@yandex.ru>
2021-01-13 23:45:04 +0500

    fix tabs contractors view

--- src/main/java/com/trade_accounting/components/ContractorsView.java
+++ src/main/java/com/trade_accounting/components/ContractorsView.java
@@ -1,43 +1,59 @@
 package com.trade_accounting.components;
 
 import com.trade_accounting.services.interfaces.ContractService;
+import com.trade_accounting.services.interfaces.ContractorService;
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.Label;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
+import com.vaadin.flow.router.AfterNavigationEvent;
+import com.vaadin.flow.router.AfterNavigationObserver;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 @Route(value = "contractors", layout = AppView.class)
 @PageTitle("Контрагенты")
-public class ContractorsView extends Div {
+public class ContractorsView extends Div implements AfterNavigationObserver {
 
     private final Div div;
 
     private final ContractService contractService;
+    private final ContractorService contractorService;
 
-    public ContractorsView(ContractService contractService) {
+    public ContractorsView(ContractService contractService,
+                           ContractorService contractorService) {
+        this.contractorService = contractorService;
         this.contractService = contractService;
         div = new Div();
-
         add(configurationSubMenu(), div);
+    }
+
+    @Override
+    public void afterNavigation(AfterNavigationEvent afterNavigationEvent) {
+            div.removeAll();
+            div.add(new ContractorsTabView(contractorService));
     }
 
     private Tabs configurationSubMenu() {
 
-        HorizontalLayout contractors = new HorizontalLayout(new Label("Контрагенты"));
-        HorizontalLayout contracts = new HorizontalLayout(new Label("Договоры"));
-        contracts.addClickListener(e -> contracts.getUI().ifPresent(ui -> {
-            div.removeAll();
-            div.add(new ContractsView(contractService));
-        }));
+        div.removeAll();
+        div.add(new ContractorsTabView(contractorService));
 
-        Tabs tabs = new Tabs(
-                new Tab(contractors),
-                new Tab(contracts)
-        );
+        Tabs tabs = new Tabs(new Tab("контрагенты"), new Tab("договоры"));
+
         tabs.setOrientation(Tabs.Orientation.HORIZONTAL);
+
+        tabs.addSelectedChangeListener(event -> {
+            String tabName = event.getSelectedTab().getLabel();
+            switch (tabName) {
+                case "контрагенты":
+                    div.removeAll();
+                    div.add(new ContractorsTabView(contractorService));
+                    break;
+                case "договоры":
+                    div.removeAll();
+                    div.add(new ContractsView(contractService));
+            }
+        });
         return tabs;
     }
 }
