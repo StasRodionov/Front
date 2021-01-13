f42ff237658e0b183c0cd33c97cf89ab27744385
frolowyan <frolowyan@gmail.com>
2021-01-13 14:25:21 +0300

    add submenu goods

--- src/main/java/com/trade_accounting/components/GoodsSubMenuView.java
+++ src/main/java/com/trade_accounting/components/GoodsSubMenuView.java
@@ -21,18 +21,26 @@ public class GoodsSubMenuView extends Div implements AfterNavigationObserver {
 
     private Tabs configurationSubMenu() {
         HorizontalLayout goodsLayout = new HorizontalLayout(new Label("Товары и услуги"));
+        HorizontalLayout realisationLayout = new HorizontalLayout(new Label("Оприходывания"));
         HorizontalLayout chargesLayout = new HorizontalLayout(new Label("Списания"));
+        HorizontalLayout interventarizationLayout = new HorizontalLayout(new Label("Инвентаризация"));
+        HorizontalLayout insideOrdersLayout = new HorizontalLayout(new Label("Внутрение заказы"));
         HorizontalLayout transferLayout = new HorizontalLayout(new Label("Перемещения"));
         HorizontalLayout priceLayout = new HorizontalLayout(new Label("Прайс-лист"));
         HorizontalLayout balanceLayout = new HorizontalLayout(new Label("Остататки"));
+        HorizontalLayout revenueLayout = new HorizontalLayout(new Label("Обороты"));
 
 
         return new Tabs(
                 new Tab(goodsLayout),
+                new Tab(realisationLayout),
                 new Tab(chargesLayout),
+                new Tab(interventarizationLayout),
+                new Tab(insideOrdersLayout),
                 new Tab(transferLayout),
                 new Tab(priceLayout),
-                new Tab(balanceLayout)
+                new Tab(balanceLayout),
+                new Tab(revenueLayout)
         );
 
     }
