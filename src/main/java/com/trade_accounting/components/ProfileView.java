d3c8ff47efcd69da05f5e6c0ed833c310fa336b3
frolowyan <frolowyan@gmail.com>
2021-01-15 18:07:07 +0300

    fixed profile view

--- src/main/java/com/trade_accounting/components/ProfileView.java
+++ src/main/java/com/trade_accounting/components/ProfileView.java
@@ -1,51 +1,36 @@
 package com.trade_accounting.components;
 
-import com.trade_accounting.models.dto.ProductDto;
 import com.trade_accounting.services.interfaces.CompanyService;
 import com.trade_accounting.services.interfaces.EmployeeService;
-import com.trade_accounting.services.interfaces.ProductService;
 import com.trade_accounting.services.interfaces.UnitService;
 import com.trade_accounting.services.interfaces.WarehouseService;
-import com.vaadin.flow.component.HasElement;
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.Label;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.AfterNavigationEvent;
 import com.vaadin.flow.router.AfterNavigationObserver;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Route(value = "profile", layout = AppView.class)
 @PageTitle("Профиль")
 public class ProfileView extends Div implements AfterNavigationObserver {
 
-
     private final UnitService unitService;
     private final CompanyService companyService;
     private final EmployeeService employeeService;
     private final WarehouseService warehouseService;
-    //TODO
-    //private final CurrencyService currencyService;
     private final Div div;
-
     public ProfileView(UnitService unitService, CompanyService companyService,
-                       EmployeeService employeeService, WarehouseService warehouseService /*CurrencyService currencyService*/) {
+                       EmployeeService employeeService, WarehouseService warehouseService) {
         this.unitService = unitService;
         this.companyService = companyService;
         this.employeeService = employeeService;
         this.warehouseService = warehouseService;
-        //this.currencyService = currencyService;
         div = new Div();
-
         add(configurationSubMenu(), div);
     }
-    HorizontalLayout companyLayout = new HorizontalLayout(new Label("Юр. лица"));
+
     @Override
     public void afterNavigation(AfterNavigationEvent afterNavigationEvent) {
         AppView appView = (AppView) afterNavigationEvent.getActiveChain().get(1);
@@ -54,53 +39,45 @@ public class ProfileView extends Div implements AfterNavigationObserver {
                 ((Tabs) e).setSelectedIndex(12);
             }
         });
-        companyLayout.getUI().ifPresent(ui -> {
+        getUI().ifPresent(ui -> {
             div.removeAll();
             div.add(new CompanyView(companyService));
         });
     }
 
     private Tabs configurationSubMenu() {
-        companyLayout.addClickListener(e ->
-                companyLayout.getUI().ifPresent(ui -> {
+        Tabs tabs = new Tabs(
+                new Tab("Юр. лица"),
+                new Tab("Сотрудники"),
+                new Tab("Склады"),
+                new Tab("Валюты"),
+                new Tab("Единицы измерения")
+        );
+        tabs.addSelectedChangeListener(event -> {
+            String tabName = event.getSelectedTab().getLabel();
+            switch (tabName) {
+                case "Юр. лица":
                     div.removeAll();
                     div.add(new CompanyView(companyService));
-                }));
-
-        HorizontalLayout employeeLayout = new HorizontalLayout(new Label("Сотрудники"));
-        employeeLayout.addClickListener(e ->
-                employeeLayout.getUI().ifPresent(ui -> {
+                    break;
+                case "Сотрудники":
                     div.removeAll();
                     div.add(new EmployeeView(employeeService));
-                }));
-
-        HorizontalLayout warehouseLayout = new HorizontalLayout(new Label("Склады"));
-        warehouseLayout.addClickListener(e ->
-                warehouseLayout.getUI().ifPresent(ui -> {
+                    break;
+                case "Склады":
                     div.removeAll();
                     div.add(new WareHouseView(warehouseService));
-                }));
-
-        HorizontalLayout currencyLayout = new HorizontalLayout(new Label("Валюты"));
-        currencyLayout.addClickListener(e ->
-                currencyLayout.getUI().ifPresent(ui -> {
-                    div.removeAll();
-                    //div.add(new CurrencyView(currenceService));
-                }));
-
-        HorizontalLayout unitLayout = new HorizontalLayout(new Label("Единицы измерения"));
-        unitLayout.addClickListener(e ->
-                unitLayout.getUI().ifPresent(ui -> {
+                    break;
+                case "Валюты":
+                    //  нужно добавить страницу для валит
+                    break;
+                case "Единицы измерения":
                     div.removeAll();
                     div.add(new UnitView(unitService));
-                }));
-
-        return new Tabs(
-                new Tab(companyLayout),
-                new Tab(employeeLayout),
-                new Tab(warehouseLayout),
-                new Tab(currencyLayout),
-                new Tab(unitLayout)
-        );
+                    break;
+                default:
+            }
+        });
+        return tabs;
     }
 }
