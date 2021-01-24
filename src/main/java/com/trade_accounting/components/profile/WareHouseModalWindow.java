151e929ddaf8236e509bd3ac170c41f67a25e4ae
sergeylukashuk <sergeylukashuk@yandex.ru>
2021-01-24 23:15:46 +0500

    add_warehouse_modal

--- src/main/java/com/trade_accounting/components/profile/WareHouseModalWindow.java
+++ src/main/java/com/trade_accounting/components/profile/WareHouseModalWindow.java
@@ -2,14 +2,18 @@ package com.trade_accounting.components.profile;
 
 import com.trade_accounting.models.dto.WarehouseDto;
 import com.trade_accounting.services.interfaces.WarehouseService;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
 
-public class WarehouseModalWindow extends Dialog {
+public class WareHouseModalWindow extends Dialog {
 
     private TextField nameField = new TextField();
 
@@ -27,8 +31,8 @@ public class WarehouseModalWindow extends Dialog {
 
     private final WarehouseService warehouseService;
 
-    public WarehouseModalWindow (WarehouseDto warehouseDto,
-                                 WarehouseService warehouseService) {
+    public WareHouseModalWindow(WarehouseDto warehouseDto,
+                                WarehouseService warehouseService) {
         this.warehouseService = warehouseService;
 
         setCloseOnOutsideClick(false);
@@ -38,85 +42,23 @@ public class WarehouseModalWindow extends Dialog {
         commentToAddressField.setValue(getFieldValueNotNull(warehouseDto.getCommentToAddress()));
         commentField.setValue(getFieldValueNotNull(warehouseDto.getComment()));
         sortNumberField.setValue(getFieldValueNotNull(warehouseDto.getSortNumber()));
-        //add(new Text("Наименование"), header(), contractorsAccordion());
+        add(new Text("Наименование"), header(),
+                new VerticalLayout(configureAddressField(), configureCommentToAddressField(),
+                        configureCommentField(),
+                        configureSortNumberField()));
     }
 
-    /*private Accordion contractorsAccordion() {
-        Accordion accordion = new Accordion();
-        VerticalLayout verticalLayout = new VerticalLayout();
-        verticalLayout.add(contractorGroupSelect(),
-                configurePhoneField(),
-                configureFaxField(),
-                configureEmailField(),
-                configureAddressField(),
-                configureCommentToAddressField(),
-                configureCommentField(),
-                configureInnField(),
-                configureSortNumberField());
-        accordion.add("О контрагенте", verticalLayout).addThemeVariants(DetailsVariant.FILLED);
-        accordion.add("Контактные лица", new VerticalLayout()).addThemeVariants(DetailsVariant.FILLED);
-        accordion.add("Реквизиты", new VerticalLayout()).addThemeVariants(DetailsVariant.FILLED);
-        accordion.add("Скидки", new VerticalLayout()).addThemeVariants(DetailsVariant.FILLED);
-        accordion.add("Доступы", new VerticalLayout()).addThemeVariants(DetailsVariant.FILLED);
-        accordion.setWidth("575px");
-        return accordion;
-    }*/
-
-    private HorizontalLayout header(){
+    private HorizontalLayout header() {
         HorizontalLayout header = new HorizontalLayout();
         nameField.setWidth("345px");
         header.add(nameField, getSaveButton(), getCancelButton());
         return header;
     }
 
-    /*private HorizontalLayout contractorGroupSelect() {
-        HorizontalLayout horizontalLayout = new HorizontalLayout();
-        Select<ContractorGroupDto> labelSelect = new Select<>();
-        labelSelect.setItems(contractorGroupService.getAll());
-        labelSelect.setItemLabelGenerator(ContractorGroupDto::getName);
-        labelSelect.setWidth(fieldWidth);
-        Label label = new Label("Группы");
-        label.setWidth(labelWidth);
-        horizontalLayout.add(label, labelSelect);
-        return horizontalLayout;
-    }*/
-
-    /*private HorizontalLayout configurePhoneField() {
-        HorizontalLayout horizontalLayout = new HorizontalLayout();
-        Label label = new Label("Телефон");
-        label.setWidth(labelWidth);
-        phoneField.setWidth(fieldWidth);
-        horizontalLayout.add(label, phoneField);
-        return horizontalLayout;
-    }*/
-
-    /*private HorizontalLayout configureFaxField() {
-        HorizontalLayout horizontalLayout = new HorizontalLayout();
-        Label label = new Label("Факс");
-        label.setWidth(labelWidth);
-        faxField.setWidth(fieldWidth);
-        horizontalLayout.add(label, faxField);
-        return horizontalLayout;
-    }*/
-
-   /* private HorizontalLayout configureEmailField() {
-        HorizontalLayout horizontalLayout = new HorizontalLayout();
-        Label label = new Label("Емейл");
-        label.setWidth(labelWidth);
-        emailField.setWidth("345px");
-        Button emailButton = new Button();
-        emailButton.setIcon(new Icon(VaadinIcon.ENVELOPE));
-        horizontalLayout.add(label, emailField, emailButton);
-        return horizontalLayout;
-    }*/
-
     private HorizontalLayout configureAddressField() {
         HorizontalLayout horizontalLayout = new HorizontalLayout();
-        Label label = new Label("Фактический адресс");
+        Label label = new Label("Фактический адрес");
         label.setWidth(labelWidth);
-        //addressField.setWidth("345px");
-        //Button emailButton = new Button();
-        //emailButton.setIcon(new Icon(VaadinIcon.CARET_DOWN));
         horizontalLayout.add(label, addressField);
         return horizontalLayout;
     }
@@ -127,7 +69,7 @@ public class WarehouseModalWindow extends Dialog {
         label.setWidth(labelWidth);
         commentToAddressField.setWidth(fieldWidth);
         commentToAddressField.getStyle().set("minHeight", "120px");
-        horizontalLayout.add(label,  commentToAddressField);
+        horizontalLayout.add(label, commentToAddressField);
         return horizontalLayout;
     }
 
@@ -137,22 +79,13 @@ public class WarehouseModalWindow extends Dialog {
         label.setWidth(labelWidth);
         commentField.setWidth(fieldWidth);
         commentField.getStyle().set("minHeight", "120px");
-        horizontalLayout.add(label,  commentField);
+        horizontalLayout.add(label, commentField);
         return horizontalLayout;
     }
 
-    /*private HorizontalLayout configureInnField() {
-        HorizontalLayout horizontalLayout = new HorizontalLayout();
-        Label label = new Label("Инн");
-        label.setWidth(labelWidth);
-        innField.setWidth(fieldWidth);
-        horizontalLayout.add(label, innField);
-        return horizontalLayout;
-    }*/
-
     private HorizontalLayout configureSortNumberField() {
         HorizontalLayout horizontalLayout = new HorizontalLayout();
-        Label label = new Label("Код");
+        Label label = new Label("Сортировочный номер");
         label.setWidth(labelWidth);
         sortNumberField.setWidth(fieldWidth);
         horizontalLayout.add(label, sortNumberField);
@@ -169,6 +102,8 @@ public class WarehouseModalWindow extends Dialog {
             newWarehouseDto.setSortNumber(sortNumberField.getValue());
             warehouseService.create(newWarehouseDto);
             close();
+
+
         });
     }
 
