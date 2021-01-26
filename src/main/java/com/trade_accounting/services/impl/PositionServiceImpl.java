66ca5fb82169c073b5b435ba19005bb305c59b9b
Sergey Grimanov <tosergi@gmail.con>
2021-01-26 12:21:38 +0300

    В PositionServiceImpl все запросы заменены на синхронные

--- src/main/java/com/trade_accounting/services/impl/PositionServiceImpl.java
+++ src/main/java/com/trade_accounting/services/impl/PositionServiceImpl.java
@@ -7,7 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import retrofit2.Call;
-import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 
@@ -34,50 +33,42 @@ public class PositionServiceImpl implements PositionService {
     public List<PositionDto> getAll() {
 
         Call<List<PositionDto>> positionDtoListCall = positionApi.getAll(positionUrl);
+        Response<List<PositionDto>> response = null;
 
-        positionDtoListCall.enqueue(new Callback<>() {
-            @Override
-            public void onResponse(Call<List<PositionDto>> call, Response<List<PositionDto>> response) {
-                if (response.isSuccessful()) {
-                    positionDtoList = response.body();
-                    log.info("Успешно выполнен запрос на получение списка PositionDto");
-                } else {
-                    log.error("Произошла ошибка при выполнении запроса на получение списка PositionDto - {}",
-                            response.errorBody());
-                }
+        try {
+            response = positionDtoListCall.execute();
+            if (response.isSuccessful()) {
+                positionDtoList = response.body();
+                log.info("Успешно выполнен запрос на получение списка PositionDto");
+            } else {
+                log.error("Произошла ошибка при выполнении запроса на получение списка PositionDto - {}", response.errorBody());
             }
 
-            @Override
-            public void onFailure(Call<List<PositionDto>> call, Throwable throwable) {
-                log.error("Произошла ошибка при получении ответа на запрос списка PositionDto", throwable);
-            }
-        });
 
+        } catch (Exception e) {
+            log.error("Произошла ошибка при выполнении запроса на получение списка PositionDto");
+        }
         return positionDtoList;
     }
 
     @Override
     public PositionDto getById(Long id) {
         Call<PositionDto> positionDtoCall = positionApi.getById(positionUrl, id);
+        Response<PositionDto> response = null;
 
-        positionDtoCall.enqueue(new Callback<>() {
-            @Override
-            public void onResponse(Call<PositionDto> call, Response<PositionDto> response) {
-                if (response.isSuccessful()) {
-                    positionDto = response.body();
-                    log.info("Успешно выполнен запрос на получение экземпляра PositionDto по id= {}", id);
-                } else {
-                    log.error("Произошла ошибка при выполнении запроса на получение экземпляра PositionDto по id= {} - {}",
-                            id, response.errorBody());
-                }
+        try {
+            response = positionDtoCall.execute();
+            if (response.isSuccessful()) {
+                positionDto = response.body();
+                log.info("Успешно выполнен запрос на получение экземпляра PositionDto по id= {}", id);
+            } else {
+                log.error("Произошла ошибка при выполнении запроса на получение экземпляра PositionDto по id= {} - {}",
+                        id, response.errorBody());
             }
 
-            @Override
-            public void onFailure(Call<PositionDto> call, Throwable throwable) {
-                log.error("Произошла ошибка при получении ответа на запрос экземпляра PositionDto по id", throwable);
-            }
-        });
-
+        } catch (Exception e) {
+            log.error("Произошла ошибка при выполнении запроса на получение экземпляра PositionDto по id");
+        }
         return positionDto;
     }
 
@@ -85,71 +76,57 @@ public class PositionServiceImpl implements PositionService {
     public void create(PositionDto positionDto) {
 
         Call<Void> positionDtoCall = positionApi.create(positionUrl, positionDto);
-
-        positionDtoCall.enqueue(new Callback<>() {
-            @Override
-            public void onResponse(Call<Void> call, Response<Void> response) {
-                if (response.isSuccessful()) {
-                    log.info("Успешно выполнен запрос на создание экземпляра PositionDto");
-                } else {
-                    log.error("Произошла ошибка при выполнении запроса на создание экземпляра PositionDto - {}",
-                            response.errorBody());
-                }
+        Response<Void> response = null;
+        try {
+            response = positionDtoCall.execute();
+            if (response.isSuccessful()) {
+                log.info("Успешно выполнен запрос на создание экземпляра PositionDto");
+            } else {
+                log.error("Произошла ошибка при выполнении запроса на создание экземпляра PositionDto - {}",
+                        response.errorBody());
             }
+            log.info("Успешно выполнен запрос на создание экземпляра PositionDto");
 
-            @Override
-            public void onFailure(Call<Void> call, Throwable throwable) {
-                log.error("Произошла ошибка при получении ответа на запрос создания экземпляра PositionDto", throwable);
-            }
-        });
-
+        } catch (Exception e) {
+            log.error("Произошла ошибка при выполнении запроса на создание экземпляра PositionDto");
+        }
     }
 
     @Override
     public void update(PositionDto positionDto) {
 
         Call<Void> positionDtoCall = positionApi.update(positionUrl, positionDto);
+        Response<Void> response = null;
 
-        positionDtoCall.enqueue(new Callback<>() {
-            @Override
-            public void onResponse(Call<Void> call, Response<Void> response) {
-                if (response.isSuccessful()) {
-                    log.info("Успешно выполнен запрос на обновление экземпляра PositionDto");
-                } else {
-                    log.error("Произошла ошибка при выполнении запроса на обновление экземпляра PositionDto - {}",
-                            response.errorBody());
-                }
+        try {
+            response = positionDtoCall.execute();
+            if (response.isSuccessful()) {
+                log.info("Успешно выполнен запрос на обновление экземпляра PositionDto");
+            } else {
+                log.error("Произошла ошибка при выполнении запроса на обновление экземпляра PositionDto - {}",
+                        response.errorBody());
             }
-
-            @Override
-            public void onFailure(Call<Void> call, Throwable throwable) {
-                log.error("Произошла ошибка при получении ответа на запрос обновления экземпляра PositionDto", throwable);
-            }
-        });
-
+        } catch (Exception e) {
+            log.error("Произошла ошибка при получении ответа на запрос обновления экземпляра PositionDto");
+        }
     }
 
     @Override
     public void deleteById(Long id) {
 
         Call<Void> positionDtoCall = positionApi.deleteById(positionUrl, id);
+        Response<Void> response = null;
 
-        positionDtoCall.enqueue(new Callback<>() {
-            @Override
-            public void onResponse(Call<Void> call, Response<Void> response) {
-                if (response.isSuccessful()) {
-                    log.info("Успешно выполнен запрос на удаление экземпляра PositionDto с id= {}", id);
-                } else {
-                    log.error("Произошла ошибка при выполнении запроса на удаление экземпляра PositionDto с id= {} - {}",
-                            id, response.errorBody());
-                }
+        try {
+            response = positionDtoCall.execute();
+            if (response.isSuccessful()) {
+                log.info("Успешно выполнен запрос на удаление экземпляра PositionDto с id= {}", id);
+            } else {
+                log.error("Произошла ошибка при выполнении запроса на удаление экземпляра PositionDto с id= {} - {}",
+                        id, response.errorBody());
             }
-
-            @Override
-            public void onFailure(Call<Void> call, Throwable throwable) {
-                log.error("Произошла ошибка при получении ответа на запрос удаления экземпляра PositionDto", throwable);
-            }
-        });
-
+        } catch (Exception e) {
+            log.error("Произошла ошибка при выполнении запроса на удаление экземпляра PositionDto");
+        }
     }
 }
