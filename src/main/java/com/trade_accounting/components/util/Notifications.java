9adeb38ee80e122b0f307365a7a32b129be4bdc3
Keyneto <Brize2302@yandex.ru>
2021-02-08 21:25:53 +0500

    add print contractors list fixes

---
+++ src/main/java/com/trade_accounting/components/util/Notifications.java
@@ -0,0 +1,53 @@
+package com.trade_accounting.components.util;
+
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H5;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.server.StreamRegistration;
+import com.vaadin.flow.server.StreamResource;
+
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+
+public class Notifications {
+
+    private final Button ok = new Button("OK");
+
+    private final VerticalLayout verticalLayout = new VerticalLayout(ok);
+
+    private final Notification notification = new Notification();
+
+    public Notifications () {
+        verticalLayout.setAlignItems(FlexComponent.Alignment.CENTER);
+        ok.addClickListener(event -> notification.close());
+        notification.setPosition(Notification.Position.BOTTOM_END);
+    }
+
+    public void InfoNotification(String message) {
+        notification.removeAll();
+        notification.add(new H5(message));
+        notification.add(verticalLayout);
+        notification.open();
+    }
+
+    public void ErrorNotification(String message) {
+        H5 text = new H5(message);
+        text.addClassName("my-style");
+        notification.removeAll();
+        notification.add(text);
+        notification.add(verticalLayout);
+
+        String styles = ".my-style { color: red; }";
+
+        StreamRegistration resource = UI.getCurrent().getSession().getResourceRegistry()
+                .registerResource(new StreamResource("styles.css", () ->
+                        new ByteArrayInputStream(styles.getBytes(StandardCharsets.UTF_8))));
+
+        UI.getCurrent().getPage().addStyleSheet(
+                "base://" + resource.getResourceUri().toString());
+        notification.open();
+    }
+}
