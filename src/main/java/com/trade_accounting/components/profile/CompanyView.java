d7c0a5e2a25ced750fd287fe61e8d62991512727
Utihiro <fadgames@yandex.ru>
2021-02-09 19:28:14 +0400

    added context menu for grid

--- src/main/java/com/trade_accounting/components/profile/CompanyView.java
+++ src/main/java/com/trade_accounting/components/profile/CompanyView.java
@@ -5,8 +5,14 @@ import com.trade_accounting.components.util.GridFilter;
 import com.trade_accounting.components.util.GridPaginator;
 import com.trade_accounting.models.dto.CompanyDto;
 import com.trade_accounting.services.interfaces.CompanyService;
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.checkbox.CheckboxGroup;
+import com.vaadin.flow.component.checkbox.CheckboxGroupVariant;
+import com.vaadin.flow.component.contextmenu.ContextMenu;
+import com.vaadin.flow.component.contextmenu.MenuItem;
+import com.vaadin.flow.component.contextmenu.SubMenu;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H2;
@@ -24,6 +30,7 @@ import com.vaadin.flow.router.Route;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Route(value = "company", layout = AppView.class)
@@ -83,6 +90,7 @@ public class CompanyView extends VerticalLayout {
         grid.getColumnByKey("sortNumber").setHeader("Нумерация").setId("Нумерация");
         grid.getColumnByKey("stamp").setHeader("Печать").setId("Печать");
         grid.getColumnByKey("legalDetailDto").setHeader("Юридические детали").setId("Юридические детали");
+        getGridContextMenu();
 
         grid.setHeight("66vh");
         grid.setColumnReorderingAllowed(true);
@@ -179,5 +187,33 @@ public class CompanyView extends VerticalLayout {
         selector.setValue("Изменить");
         selector.setWidth("130px");
         return selector;
+    }
+
+    private ContextMenu getGridContextMenu() {
+        ContextMenu contextMenu = new ContextMenu();
+        contextMenu.setTarget(grid);
+
+        CheckboxGroup<Grid.Column<CompanyDto>> checkboxGroup = new CheckboxGroup<>();
+        contextMenu.addItem(checkboxGroup);
+
+        checkboxGroup.setItems(grid.getColumns().stream());
+        checkboxGroup.addThemeVariants(CheckboxGroupVariant.LUMO_VERTICAL);
+        checkboxGroup.setItemLabelGenerator(i -> i.getId().orElse(i.getKey()));
+        checkboxGroup.setValue(grid.getColumns().stream().filter(Component::isVisible).collect(Collectors.toSet()));
+        checkboxGroup.addSelectionListener(e -> {
+            e.getAddedSelection().forEach(i -> i.setVisible(true));
+            e.getRemovedSelection().forEach(i -> i.setVisible(false));
+        });
+
+        MenuItem menuItem = contextMenu.addItem("Количество строк");
+        SubMenu subMenu = menuItem.getSubMenu();
+
+        subMenu.addItem("25", e -> paginator.setItemsPerPage(25));
+
+        subMenu.addItem("50", e -> paginator.setItemsPerPage(50));
+
+        subMenu.addItem("100", e -> paginator.setItemsPerPage(100));
+
+        return contextMenu;
     }
 }
