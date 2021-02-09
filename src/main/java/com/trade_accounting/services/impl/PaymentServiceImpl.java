ee6a6879c5e078a30d5e5139c5fdf3586eeeffe5
Leonid Volobuev <14leonidas88@gmail.com>
2021-02-10 01:25:49 +0500

    add service

---
+++ src/main/java/com/trade_accounting/services/impl/PaymentServiceImpl.java
@@ -0,0 +1,94 @@
+package com.trade_accounting.services.impl;
+
+import com.trade_accounting.models.dto.PaymentDto;
+import com.trade_accounting.services.interfaces.PaymentService;
+import com.trade_accounting.services.interfaces.api.PaymentApi;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Slf4j
+public class PaymentServiceImpl implements PaymentService {
+
+    private final PaymentApi paymentApi;
+    private final String paymentUrl;
+
+    public PaymentServiceImpl(@Value("${payment_url}") String paymentUrl, Retrofit retrofit) {
+        paymentApi = retrofit.create(PaymentApi.class);
+        this.paymentUrl = paymentUrl;
+    }
+
+    @Override
+    public List<PaymentDto> getAll() {
+        List<PaymentDto> paymentDtoList = new ArrayList<>();
+        Call<List<PaymentDto>> paymentDtoListCall = paymentApi.getAll(paymentUrl);
+
+        try {
+            paymentDtoList.addAll(paymentDtoListCall.execute().body());
+            log.info("Успешно выполнен запрос на получение списка PaymentDto");
+        } catch (IOException e) {
+            log.error("Произошла ошибка при выполнении запроса на получение списка PaymentDto - {}", e);
+        }
+        return paymentDtoList;
+    }
+
+    @Override
+    public PaymentDto getById(Long id) {
+        PaymentDto paymentDto = null;
+        Call<PaymentDto> paymentDtoCall = paymentApi.getById(paymentUrl, id);
+
+        try {
+            paymentDto = paymentDtoCall.execute().body();
+            log.info("Успешно выполнен запрос на получение экземпляра PaymentDto с id = {}", id);
+        } catch (IOException e) {
+            log.error("Произошла ошибка при выполнении запроса на получение экземпляра PaymentDto по id= {} - {}", id, e);
+        }
+        return paymentDto;
+    }
+
+    @Override
+    public void create(PaymentDto paymentDto) {
+
+        Call<Void> paymentDtoCall = paymentApi.create(paymentUrl, paymentDto);
+
+        try {
+            paymentDtoCall.execute();
+            log.info("Успешно выполнен запрос на создание экземпляра PaymentDto");
+        } catch (IOException e) {
+            log.error("Произошла ошибка при выполнении запроса на создание экземпляра PaymentDto - {}", e);
+        }
+    }
+
+    @Override
+    public void update(PaymentDto paymentDto) {
+
+        Call<Void> paymentDtoCall = paymentApi.update(paymentUrl, paymentDto);
+
+        try {
+            paymentDtoCall.execute();
+            log.info("Успешно выполнен запрос на обновление экземпляра PaymentDto");
+        } catch (IOException e) {
+            log.error("Произошла ошибка при выполнении запроса на обновление экземпляра PaymentDto - {}", e);
+        }
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+        Call<Void> paymentDtoCall = paymentApi.deleteById(paymentUrl, id);
+
+        try {
+            paymentDtoCall.execute();
+            log.info("Успешно выполнен запрос на удаление экземпляра PaymentDto с id = {}", id);
+        } catch (IOException e) {
+            log.error("Произошла ошибка при выполнении запроса на удаление экземпляра PaymentDto по id= {} - {}", id, e);
+        }
+    }
+}
