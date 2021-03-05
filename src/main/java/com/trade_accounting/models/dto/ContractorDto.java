1d47f2205b08ffcc0c590c8a86da750f1991a28b
Alex Alex <lex156166@gmail.com>
2021-03-05 16:49:24 +0400

    Contractor filter
    A filter has been implemented on the page contractors, according to the fields that are available today.

--- src/main/java/com/trade_accounting/models/dto/ContractorDto.java
+++ src/main/java/com/trade_accounting/models/dto/ContractorDto.java
@@ -1,6 +1,5 @@
 package com.trade_accounting.models.dto;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,32 +12,18 @@ import java.util.List;
 public class ContractorDto {
 
     private Long id;
-
     private String name;
-
     private String inn;
-
     private String sortNumber;
-
     private String phone;
-
     private String fax;
-
     private String email;
-
     private String address;
-
     private String commentToAddress;
-
     private String comment;
-
     private ContractorGroupDto contractorGroupDto;
-
     private TypeOfContractorDto typeOfContractorDto;
-
     private TypeOfPriceDto typeOfPriceDto;
-
     private List<BankAccountDto> bankAccountDto;
-
     private LegalDetailDto legalDetailDto;
 }
