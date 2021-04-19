241dd2e6b64c31a7b53cd409acb5bfbbaf5360eb
Ivan Saushin <ivan.saushin@gmail.com>
2021-04-19 19:42:17 +0300

    added adding an xls template

--- src/main/java/com/trade_accounting/components/sells/SalesSubCustomersOrdersView.java
+++ src/main/java/com/trade_accounting/components/sells/SalesSubCustomersOrdersView.java
@@ -11,23 +11,29 @@ import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.component.textfield.NumberField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.textfield.TextFieldVariant;
+import com.vaadin.flow.component.upload.Upload;
+import com.vaadin.flow.component.upload.receivers.MemoryBuffer;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.router.AfterNavigationEvent;
 import com.vaadin.flow.router.AfterNavigationObserver;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.StreamRegistration;
 import com.vaadin.flow.server.StreamResource;
 import com.vaadin.flow.spring.annotation.SpringComponent;
 import com.vaadin.flow.spring.annotation.UIScope;
@@ -35,8 +41,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 
+import javax.swing.text.html.Option;
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -229,12 +240,48 @@ public class SalesSubCustomersOrdersView extends VerticalLayout implements After
 
     private Select<String> valuePrint() {
         Select<String> print = new Select<>();
-        print.setItems("Печать");
+        print.setItems("Печать", "Добавить шаблон");
         print.setValue("Печать");
         getXlsFile().forEach(x -> print.add(getLinkToSalesXls(x)));
+        uploadXlsTemplates(print);
         print.setWidth("130px");
         return print;
     }
+
+    private void uploadXlsTemplates(Select<String> print) {
+        Dialog dialog = new Dialog();
+        MemoryBuffer buffer = new MemoryBuffer();
+        Upload upload = new Upload(buffer);
+        configureUploadFinishedListener(upload, buffer, dialog, print);
+        dialog.add(upload);
+        print.addValueChangeListener(x -> {
+            if (x.getValue().equals("Добавить шаблон")) {
+                dialog.open();
+            }
+        });
+    }
+
+    private void configureUploadFinishedListener(Upload upload, MemoryBuffer buffer, Dialog dialog, Select<String> print) {
+        upload.addFinishedListener(event -> {
+            if (getXlsFile().stream().map(File::getName).anyMatch(x -> x.equals(event.getFileName()))) {
+                getErrorNotification("Файл с таки именем уже существует");
+            } else {
+                File exelTemplate = new File(pathForSaveSalesXlsTemplate + event.getFileName());
+                try (FileOutputStream fos = new FileOutputStream(exelTemplate)) {
+                    fos.write(buffer.getInputStream().readAllBytes());
+                    print.removeAll();
+                    getXlsFile().forEach(x -> print.add(getLinkToSalesXls(x)));
+                    log.info("xls шаблон успешно загружен");
+                    getInfoNotification("Файл успешно загружен");
+                } catch (IOException e) {
+                    getErrorNotification("При загрузке шаблона произошла ошибка");
+                    log.error("при загрузке xls шаблона произошла ошибка");
+                }
+                dialog.close();
+            }
+        });
+    }
+
 
     private List<File> getXlsFile() {
         File dir = new File(pathForSaveSalesXlsTemplate);
@@ -292,6 +339,27 @@ public class SalesSubCustomersOrdersView extends VerticalLayout implements After
     @Override
     public void afterNavigation(AfterNavigationEvent afterNavigationEvent) {
         updateList();
+    }
+
+    private void getInfoNotification(String message) {
+        Notification notification = new Notification(message, 5000);
+        notification.open();
+    }
+
+    private void getErrorNotification(String message) {
+        Div content = new Div();
+        content.addClassName("my-style");
+        content.setText(message);
+        Notification notification = new Notification(content);
+        notification.setDuration(5000);
+        String styles = ".my-style { color: red; }";
+        StreamRegistration resource = UI.getCurrent().getSession()
+                .getResourceRegistry()
+                .registerResource(new StreamResource("styles.css", () ->
+                        new ByteArrayInputStream(styles.getBytes(StandardCharsets.UTF_8))));
+        UI.getCurrent().getPage().addStyleSheet(
+                "base://" + resource.getResourceUri().toString());
+        notification.open();
     }
 }
 
