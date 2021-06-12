5f25d0ff8cfdc939b3c22779f3fd945533f2ab7f
krasnovss91 <krasnovstanislav91@gmail.com>
2021-06-12 14:12:49 +0400

    ServiceImpl_2

--- src/main/java/com/trade_accounting/services/impl/RemainServiceImpl.java
+++ src/main/java/com/trade_accounting/services/impl/RemainServiceImpl.java
@@ -2,10 +2,26 @@ package com.trade_accounting.services.impl;
 
 import com.trade_accounting.models.dto.RemainDto;
 import com.trade_accounting.services.interfaces.RemainService;
+import com.trade_accounting.services.interfaces.api.RemainApi;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import retrofit2.Retrofit;
 
 import java.util.List;
 
+@Service
+@Slf4j
 public class RemainServiceImpl implements RemainService {
+
+    private final RemainApi remainApi;
+    private final String remainUrl;
+
+    public RemainServiceImpl(@Value("@{remain_url}") String remainUrl, Retrofit retrofit){
+        remainApi = retrofit.create(RemainApi.class);
+        this.remainUrl = remainUrl;
+    }
+
     @Override
     public List<RemainDto> getAll() {
         return null;
