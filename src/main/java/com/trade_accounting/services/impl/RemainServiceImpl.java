ec5e67e0266f7a30408b8991ff081307497a16b0
krasnovss91 <krasnovstanislav91@gmail.com>
2021-06-12 14:17:19 +0400

    ServiceImpl_3

--- src/main/java/com/trade_accounting/services/impl/RemainServiceImpl.java
+++ src/main/java/com/trade_accounting/services/impl/RemainServiceImpl.java
@@ -6,8 +6,11 @@ import com.trade_accounting.services.interfaces.api.RemainApi;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import retrofit2.Call;
 import retrofit2.Retrofit;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -17,14 +20,23 @@ public class RemainServiceImpl implements RemainService {
     private final RemainApi remainApi;
     private final String remainUrl;
 
-    public RemainServiceImpl(@Value("@{remain_url}") String remainUrl, Retrofit retrofit){
+    public RemainServiceImpl(@Value("@{remain_url}") String remainUrl, Retrofit retrofit) {
         remainApi = retrofit.create(RemainApi.class);
         this.remainUrl = remainUrl;
     }
 
     @Override
     public List<RemainDto> getAll() {
-        return null;
+        List<RemainDto> remainDtoList = new ArrayList<>();
+        Call<List<RemainDto>> remainDtoListCall = remainApi.getAll(remainUrl);
+
+        try {
+            remainDtoList.addAll(remainDtoListCall.execute().body());
+            log.info("Успешно выполнен запрос на получение списка RemainDto");
+        } catch (IOException e) {
+            log.error("Произошла ошибка при выполнении запроса на получение списка RemainDto - {}", e);
+        }
+        return remainDtoList;
     }
 
     @Override
