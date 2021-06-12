584a30aaa99f921e549e545448cc4789c38fddf7
krasnovss91 <krasnovstanislav91@gmail.com>
2021-06-12 14:08:01 +0400

    ServiceImpl_1

--- src/main/java/com/trade_accounting/services/impl/RemainServiceImpl.java
+++ src/main/java/com/trade_accounting/services/impl/RemainServiceImpl.java
@@ -1,4 +1,33 @@
 package com.trade_accounting.services.impl;
 
-public class RemainServiceImpl {
+import com.trade_accounting.models.dto.RemainDto;
+import com.trade_accounting.services.interfaces.RemainService;
+
+import java.util.List;
+
+public class RemainServiceImpl implements RemainService {
+    @Override
+    public List<RemainDto> getAll() {
+        return null;
+    }
+
+    @Override
+    public RemainDto getById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void create(RemainDto remainDto) {
+
+    }
+
+    @Override
+    public void update(RemainDto remainDto) {
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
 }
