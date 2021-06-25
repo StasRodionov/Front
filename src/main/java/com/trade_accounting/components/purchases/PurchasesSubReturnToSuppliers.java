dd44d31723edf52d53134d0065e78a57b558f0be
Dmitry Evstratov <evstratovdmitry1992@gmail.com>
2021-06-25 12:35:11 +0300

    updated cls PurchasesSubReturnToSuppliers, PurchasesSubMenuView

--- src/main/java/com/trade_accounting/components/purchases/PurchasesSubReturnToSuppliers.java
+++ src/main/java/com/trade_accounting/components/purchases/PurchasesSubReturnToSuppliers.java
@@ -1,7 +1,95 @@
 package com.trade_accounting.components.purchases;
 
-public class PurchasesSubReturnToSuppliers {
+import com.trade_accounting.components.AppView;
+import com.trade_accounting.components.util.GridFilter;
+import com.trade_accounting.components.util.GridPaginator;
+import com.trade_accounting.components.util.Notifications;
+import com.trade_accounting.models.dto.ReturnToSupplierDto;
+import com.trade_accounting.services.interfaces.CompanyService;
+import com.trade_accounting.services.interfaces.ContractService;
+import com.trade_accounting.services.interfaces.ContractorService;
+import com.trade_accounting.services.interfaces.ReturnToSupplierService;
+import com.trade_accounting.services.interfaces.WarehouseService;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.AfterNavigationEvent;
+import com.vaadin.flow.router.AfterNavigationObserver;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.spring.annotation.SpringComponent;
+import com.vaadin.flow.spring.annotation.UIScope;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 
+import java.awt.*;
+import java.util.List;
 
+@Slf4j
+@Route(value = "returnsToSuppliers", layout = AppView.class)
+@PageTitle("Возвраты поставщикам")
+@SpringComponent
+@UIScope
+public class PurchasesSubReturnToSuppliers extends VerticalLayout implements AfterNavigationObserver {
 
+    private final ReturnToSupplierService returnToSupplierService;
+    private final WarehouseService warehouseService;
+    private final CompanyService companyService;
+    private final ContractorService contractorService;
+    private final ContractService contractService;
+    private final Notifications notifications;
+    private final List<ReturnToSupplierDto> returnToSupplierDtoList;
+    private HorizontalLayout horizontalLayout;
+    private final Grid<ReturnToSupplierDto> grid = new Grid<>(ReturnToSupplierDto.class, false);
+    private GridPaginator<ReturnToSupplierDto> paginator;
+    private final GridFilter<ReturnToSupplierDto> filter;
+
+    @Autowired
+    public PurchasesSubReturnToSuppliers(ReturnToSupplierService returnToSupplierService, WarehouseService warehouseService,
+                                         CompanyService companyService, ContractorService contractorService, ContractService contractService,
+                                         @Lazy Notifications notifications, List<ReturnToSupplierDto> returnToSupplierDtoList) {
+        this.returnToSupplierService = returnToSupplierService;
+        this.warehouseService = warehouseService;
+        this.companyService = companyService;
+        this.contractorService = contractorService;
+        this.contractService = contractService;
+        this.notifications = notifications;
+        this.returnToSupplierDtoList = returnToSupplierDtoList;
+        this.filter = new GridFilter<>(grid);
+        configureActions();
+        add(horizontalLayout);
+    }
+
+    private void configureActions(){
+        horizontalLayout = new HorizontalLayout();
+        horizontalLayout.add(buttonQuestion(), title());
+        horizontalLayout.setDefaultVerticalComponentAlignment(Alignment.CENTER);
+    }
+
+    private List<ReturnToSupplierDto> loadReturnToSuppliers(){
+        return returnToSupplierService.getAll();
+    }
+
+    private H2 title(){
+        H2 title = new H2("Возвраты поставщикам");
+        title.setHeight("2.2em");
+        return title;
+    }
+
+    private com.vaadin.flow.component.button.Button buttonQuestion() {
+        com.vaadin.flow.component.button.Button buttonQuestion = new Button(new Icon(VaadinIcon.QUESTION_CIRCLE_O));
+        buttonQuestion.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+        return buttonQuestion;
+    }
+
+    @Override
+    public void afterNavigation(AfterNavigationEvent afterNavigationEvent) {
+
+    }
 }
