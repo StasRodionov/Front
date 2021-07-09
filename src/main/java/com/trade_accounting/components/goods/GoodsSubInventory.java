04adc6d90fd68b432913cc6f1b9e76673473a983
Alexey <slag88@mail.ru>
2021-07-09 12:26:05 +0300

    add api service model and view table Inventarization

--- src/main/java/com/trade_accounting/components/goods/GoodsSubInventory.java
+++ src/main/java/com/trade_accounting/components/goods/GoodsSubInventory.java
@@ -1,14 +1,24 @@
 package com.trade_accounting.components.goods;
 
 import com.trade_accounting.components.AppView;
+import com.trade_accounting.components.util.GridPaginator;
+import com.trade_accounting.components.util.Notifications;
+import com.trade_accounting.models.dto.CorrectionDto;
+import com.trade_accounting.models.dto.InventarizationDto;
+import com.trade_accounting.services.interfaces.CompanyService;
+import com.trade_accounting.services.interfaces.InventarizationService;
+import com.trade_accounting.services.interfaces.WarehouseService;
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.Shortcuts;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.menubar.MenuBar;
@@ -18,6 +28,7 @@ import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.component.textfield.NumberField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.textfield.TextFieldVariant;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -37,16 +48,32 @@ public class GoodsSubInventory extends VerticalLayout {
 
     private final TextField textField = new TextField();
     private final MenuBar selectXlsTemplateButton = new MenuBar();
+    private final WarehouseService warehouseService;
+    private final CompanyService companyService;
+    private final List<InventarizationDto> data;
+    private final InventarizationService inventarizationService;
+    private final Grid<InventarizationDto> grid = new Grid<>(InventarizationDto.class, false);
+    private GridPaginator<InventarizationDto> paginator;
+    private Notifications notifications;
 
     @Autowired
-    public GoodsSubInventory() {
-        add(configureActions());
+    public GoodsSubInventory(WarehouseService warehouseService,
+                             CompanyService companyService,
+                             InventarizationService inventarizationService,
+                             Notifications notifications) {
+        this.warehouseService = warehouseService;
+        this.companyService = companyService;
+        this.inventarizationService = inventarizationService;
+        this.data = getData();
+        paginator = new GridPaginator<>(grid, data, 50);
+        this.notifications = notifications;
+        add(configureActions(), grid, paginator);
         configureGrid();
         setSizeFull();
     }
 
-    private void configureGrid() {
-
+    private List<InventarizationDto> getData() {
+        return inventarizationService.getAll();
     }
 
     private HorizontalLayout configureActions() {
@@ -128,6 +155,14 @@ public class GoodsSubInventory extends VerticalLayout {
         select.setItems(stringList);
         select.setValue("Изменить");
         select.setWidth("130px");
+        select.addValueChangeListener(event -> {
+            if (select.getValue().equals("Удалить")) {
+                deleteSelectedInventarizations();
+                grid.deselectAll();
+                select.setValue("Изменить");
+                paginator.setData(getData());
+            }
+        });
         return select;
     }
 
@@ -157,4 +192,40 @@ public class GoodsSubInventory extends VerticalLayout {
         return textOrder;
     }
 
+    private void configureGrid() {
+        grid.addColumn("id").setHeader("№").setId("№");
+        grid.addColumn(InventarizationDto::getDate).setKey("date").setHeader("Дата").setSortable(true);
+        grid.addColumn(inventarizationDto -> warehouseService.getById(inventarizationDto.getWarehouseId())
+                .getName()).setKey("warehouse").setHeader("Склад").setId("Склад");
+        grid.addColumn(inventarizationDto -> companyService.getById(inventarizationDto.getCompanyId())
+                .getName()).setKey("company").setHeader("Компания").setId("Компания");
+        grid.addColumn(new ComponentRenderer<>(this::getIsSentIcon)).setKey("sent").setHeader("Отправлено")
+                .setId("Отправлено");
+        grid.addColumn("comment").setHeader("Комментарий").setId("Комментарий");
+        grid.setHeight("66vh");
+        grid.setMaxWidth("2500px");
+        grid.setColumnReorderingAllowed(true);
+        grid.setSelectionMode(Grid.SelectionMode.MULTI);
+    }
+
+    private Component getIsSentIcon(InventarizationDto inventarizationDto) {
+        if (inventarizationDto.getStatus()) {
+            Icon icon = new Icon(VaadinIcon.CHECK);
+            icon.setColor("green");
+            return icon;
+        } else {
+            return new Span("");
+        }
+    }
+
+    private void deleteSelectedInventarizations() {
+        if (!grid.getSelectedItems().isEmpty()) {
+            for (InventarizationDto inventarizationDto : grid.getSelectedItems()) {
+                inventarizationService.deleteById(inventarizationDto.getId());
+                notifications.infoNotification("Выбранные заказы успешно удалены");
+            }
+        } else {
+            notifications.errorNotification("Сначала отметьте галочками нужные заказы");
+        }
+    }
 }
