67b3d004be95073eaf290a5d02a61d5bfaad2478
lvv-easy <levin263825@gmail.com>
2021-09-21 10:57:53 +0300

    dev_hot_fix

--- src/main/java/com/trade_accounting/components/retail/RetailView.java
+++ src/main/java/com/trade_accounting/components/retail/RetailView.java
@@ -7,6 +7,7 @@ import com.trade_accounting.services.interfaces.CompanyService;
 import com.trade_accounting.services.interfaces.ContractorService;
 import com.trade_accounting.services.interfaces.EmployeeService;
 import com.trade_accounting.services.interfaces.PayoutService;
+import com.trade_accounting.services.interfaces.RetailOperationWithPointsService;
 import com.trade_accounting.services.interfaces.RetailReturnsService;
 import com.trade_accounting.services.interfaces.RetailSalesService;
 import com.trade_accounting.services.interfaces.RetailStoreService;
@@ -29,6 +30,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class RetailView extends Div implements AfterNavigationObserver {
 
     private final Div div;
+    private final RetailOperationWithPointsService retailOperationWithPointsService;
+    private final BonusProgramService bonusProgramService;
+    private final TaskService taskService;
+    private final ContractorService contractorService;
     private final RetailStoreService retailStoreService;
     private final RetailSalesService retailSalesService;
     private final CompanyService companyService;
@@ -38,9 +43,13 @@ public class RetailView extends Div implements AfterNavigationObserver {
     private final RetailReturnsService retailReturnsService;
 
     @Autowired
-    public RetailView(RetailStoreService retailStoreService, RetailSalesService retailSalesService,
+    public RetailView(RetailOperationWithPointsService retailOperationWithPointsService, BonusProgramService bonusProgramService, TaskService taskService, ContractorService contractorService, RetailStoreService retailStoreService, RetailSalesService retailSalesService,
                       CompanyService companyService, EmployeeService employeeService,
                       PayoutService payoutService, Notifications notifications, RetailReturnsService retailReturnsService) {
+        this.retailOperationWithPointsService = retailOperationWithPointsService;
+        this.bonusProgramService = bonusProgramService;
+        this.taskService = taskService;
+        this.contractorService = contractorService;
         this.retailStoreService = retailStoreService;
         this.retailSalesService = retailSalesService;
         this.companyService = companyService;
