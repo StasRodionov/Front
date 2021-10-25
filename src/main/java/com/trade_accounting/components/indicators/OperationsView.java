550809ef5b3b78a6a82282955a7af253b8943674
Denisov Sergey <sedenisov@mail.ru>
2021-10-25 11:44:32 +0300

    front-> исправлена навигация добавления документов

--- src/main/java/com/trade_accounting/components/indicators/OperationsView.java
+++ src/main/java/com/trade_accounting/components/indicators/OperationsView.java
@@ -1,13 +1,23 @@
 package com.trade_accounting.components.indicators;
 
 import com.trade_accounting.components.AppView;
-import com.trade_accounting.components.goods.TitleForModal;
+import com.trade_accounting.components.goods.GoodsSubInventoryModalWindow;
+import com.trade_accounting.components.goods.InternalOrderModalView;
+import com.trade_accounting.components.goods.LossModalWindow;
+import com.trade_accounting.components.goods.MovementViewModalWindow;
+import com.trade_accounting.components.goods.PostingModal;
+import com.trade_accounting.components.money.CreditOrderModal;
+import com.trade_accounting.components.money.ExpenseOrderModal;
+import com.trade_accounting.components.money.IncomingPaymentModal;
+import com.trade_accounting.components.money.OutgoingPaymentModal;
+import com.trade_accounting.components.purchases.AcceptanceModalView;
+import com.trade_accounting.components.purchases.SupplierAccountModalView;
+import com.trade_accounting.components.sells.SalesEditCreateInvoiceView;
+import com.trade_accounting.components.sells.SalesEditShipmentView;
+import com.trade_accounting.components.util.GridPaginator;
 import com.trade_accounting.components.util.Notifications;
-import com.trade_accounting.services.interfaces.CompanyService;
-import com.trade_accounting.services.interfaces.InternalOrderProductsDtoService;
-import com.trade_accounting.services.interfaces.InternalOrderService;
-import com.trade_accounting.services.interfaces.ProductService;
-import com.trade_accounting.services.interfaces.WarehouseService;
+import com.trade_accounting.models.dto.OperationsDto;
+import com.trade_accounting.services.interfaces.OperationsService;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.Shortcuts;
@@ -17,6 +27,7 @@ import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.contextmenu.MenuItem;
 import com.vaadin.flow.component.contextmenu.SubMenu;
 import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.icon.Icon;
@@ -45,12 +56,65 @@ import java.util.List;
 @PageTitle("Документы")
 @UIScope
 public class OperationsView extends VerticalLayout {
+    private final OperationsService operationsService;
+    private final CreditOrderModal creditOrderModal;
+    private final SalesEditCreateInvoiceView salesEditCreateInvoiceView;
+    private final GoodsSubInventoryModalWindow goodsSubInventoryModalWindow;
+    private final InternalOrderModalView internalOrderModalView;
+    private final LossModalWindow lossModalWindow;
+    private final MovementViewModalWindow movementViewModalWindow;
+    private final PostingModal postingModal;
+    private final IncomingPaymentModal incomingPaymentModal;
+    private final OutgoingPaymentModal outgoingPaymentModal;
+    private final ExpenseOrderModal expenseOrderModal;
+    private final SupplierAccountModalView supplierAccountModalView;
+    private final AcceptanceModalView acceptanceModalView;
+    private final SalesEditShipmentView salesEditShipmentView;
 
 
-    public OperationsView() {
+//    private final Grid<OperationsDto> grid = new Grid<>(OperationsDto.class, false);
+//    private final GridPaginator<OperationsDto> paginator;
+    private final Notifications notifications;
+    public OperationsView(OperationsService operationsService, CreditOrderModal creditOrderModal,
+                          SalesEditCreateInvoiceView salesEditCreateInvoiceView,
+                          GoodsSubInventoryModalWindow goodsSubInventoryModalWindow,
+                          InternalOrderModalView internalOrderModalView,
+                          LossModalWindow lossModalWindow, MovementViewModalWindow movementViewModalWindow,
+                          PostingModal postingModal, IncomingPaymentModal incomingPaymentModal,
+                          OutgoingPaymentModal outgoingPaymentModal, ExpenseOrderModal expenseOrderModal,
+                          SupplierAccountModalView supplierAccountModalView, AcceptanceModalView acceptanceModalView, SalesEditShipmentView salesEditShipmentView, Notifications notifications) {
+        this.operationsService = operationsService;
+        this.creditOrderModal = creditOrderModal;
+        this.salesEditCreateInvoiceView = salesEditCreateInvoiceView;
+        this.goodsSubInventoryModalWindow = goodsSubInventoryModalWindow;
+        this.internalOrderModalView = internalOrderModalView;
+        this.lossModalWindow = lossModalWindow;
+        this.movementViewModalWindow = movementViewModalWindow;
+        this.postingModal = postingModal;
+        this.incomingPaymentModal = incomingPaymentModal;
+        this.outgoingPaymentModal = outgoingPaymentModal;
+        this.expenseOrderModal = expenseOrderModal;
+        this.supplierAccountModalView = supplierAccountModalView;
+        this.acceptanceModalView = acceptanceModalView;
+        this.salesEditShipmentView = salesEditShipmentView;
+        this.notifications = notifications;
+//        List<OperationsDto> data = getData();
+//        paginator = new GridPaginator<>(grid, data, 50);
         setHorizontalComponentAlignment(Alignment.CENTER);
         add(getUpperLayout());
+//        configureGrid();
     }
+
+//    private Grid<OperationsDto> configureGrid() {
+//        grid.addColumn(OperationsDto::getDate).setHeader("Дата документа").setId("Дата документа");
+//        grid.addColumn(OperationsDto::getId).setHeader("№").setId("№");
+//        grid.addColumn(OperationsDto::getComment).setHeader("Комментарий").setId("Комментарий");
+//        return grid;
+//    }
+
+//    private List<OperationsDto> getData() {
+//        return operationsService.getAll();
+//    }
 
     private Component getUpperLayout(){
         HorizontalLayout mainLayout = new HorizontalLayout();
@@ -145,69 +209,40 @@ public class OperationsView extends VerticalLayout {
         MenuItem operationsMenuItem = menuBar.addItem("Добавить документ");
         SubMenu operationsSubMenu = operationsMenuItem.getSubMenu();
 
-        //Продажи
+        //Продажи sells/shipment-edit
         MenuItem salesSubCustomersOrders = operationsSubMenu.addItem("Заказ покупателя");
         salesSubCustomersOrders.addClickListener(e -> operations.getUI()
-                .ifPresent(ui -> ui.navigate("salesSubCustomersOrdersModal")));
+                .ifPresent(ui -> ui.navigate("sells/customer-order-edit")));
 
         MenuItem salesSubInvoicesToBuyers = operationsSubMenu.addItem("Счета покупателей");
         salesSubInvoicesToBuyers.addClickListener(e -> operations.getUI()
-                .ifPresent(ui -> ui.navigate("salesSubInvoicesToBuyersModal")));
-
+                .ifPresent(ui -> ui.navigate("sells/add-new-invoices-to-buyers")));
         MenuItem salesSubShipment = operationsSubMenu.addItem("Отгрузки");
         salesSubShipment.addClickListener(e -> operations.getUI()
-                .ifPresent(ui -> ui.navigate("salesSubShipmentModal")));
-
+                .ifPresent(ui -> ui.navigate("sells/shipment-edit")));
         //Закупки
         MenuItem supplierOrders = operationsSubMenu.addItem("Заказы поставщикам");
         supplierOrders.addClickListener(e -> operations.getUI()
-                .ifPresent(ui -> ui.navigate("supplierOrdersModal")));
+                .ifPresent(ui -> ui.navigate("sells/customer-order-edit")));
 
-        MenuItem vendorAccounts = operationsSubMenu.addItem("Счета поставщиков");
-        vendorAccounts.addClickListener(e -> operations.getUI()
-                .ifPresent(ui -> ui.navigate("vendorAccountsModal")));
-
-        MenuItem admissions = operationsSubMenu.addItem("Приемка");
-        admissions.addClickListener(e -> operations.getUI()
-                .ifPresent(ui -> ui.navigate("admissionsModal")));
+        MenuItem vendorAccounts = operationsSubMenu.addItem("Счета поставщиков", menuItemClickEvent -> supplierAccountModalView.open());
+        MenuItem admissions = operationsSubMenu.addItem("Приемка", menuItemClickEvent -> acceptanceModalView.open());
 
         //Деньги
-
-        MenuItem incomingPaymentModal = operationsSubMenu.addItem("Входящие платежи");
-        incomingPaymentModal.addClickListener(e -> operations.getUI()
-                .ifPresent(ui -> ui.navigate("incomingPaymentModal")));
-
-        MenuItem сreditOrder = operationsSubMenu.addItem("Приходные ордеры");
-        сreditOrder.addClickListener(e -> operations.getUI()
-                .ifPresent(ui -> ui.navigate("сreditOrderModal")));
-
-        MenuItem outComingPaymentModal = operationsSubMenu.addItem("Исходящие платежи");
-        outComingPaymentModal.addClickListener(e -> operations.getUI()
-                .ifPresent(ui -> ui.navigate("outComingPaymentModal")));
-
-        MenuItem expenseOrder = operationsSubMenu.addItem("Расходные ордеры");
-        expenseOrder.addClickListener(e -> operations.getUI()
-                .ifPresent(ui -> ui.navigate("expenseOrderModal")));
+        MenuItem incomingPayment = operationsSubMenu.addItem("Входящие платежи", menuItemClickEvent -> incomingPaymentModal.open());
+        MenuItem сreditOrder = operationsSubMenu.addItem("Приходные ордеры", menuItemClickEvent -> creditOrderModal.open());
+        MenuItem outComingPayment = operationsSubMenu.addItem("Исходящие платежи", menuItemClickEvent -> outgoingPaymentModal.open());
+        MenuItem expenseOrder = operationsSubMenu.addItem("Расходные ордеры", menuItemClickEvent -> expenseOrderModal.open());
 
         //Склад
-        MenuItem internalOrder = operationsSubMenu.addItem("Внутренний заказ");
-        internalOrder.addClickListener(e -> operations.getUI()
-                .ifPresent(ui -> ui.navigate("internalOrderModal")));
-
+        MenuItem internalOrder = operationsSubMenu.addItem("Внутренний заказ", menuItemClickEvent -> internalOrderModalView.open());
         MenuItem movement = operationsSubMenu.addItem("Перемещение");
         movement.addClickListener(e -> operations.getUI().ifPresent(ui -> ui.navigate("goods/add_moving")));
-
-        MenuItem inventory = operationsSubMenu.addItem("Инвентаризация");
-        inventory.addClickListener(e -> operations.getUI().ifPresent(ui -> ui.navigate("goods/inventory")));
-
-        MenuItem posting = operationsSubMenu.addItem("Оприходование");
-        posting.addClickListener(e -> operations.getUI().ifPresent(ui -> ui.navigate("postingModal")));
-
-        MenuItem loss = operationsSubMenu.addItem("Списание");
-        loss.addClickListener(e -> operations.getUI().ifPresent(ui -> ui.navigate("lossModal")));
+        MenuItem inventory = operationsSubMenu.addItem("Инвентаризация", menuItemClickEvent -> goodsSubInventoryModalWindow.open());
+        MenuItem posting = operationsSubMenu.addItem("Оприходование", menuItemClickEvent -> postingModal.open());
+        MenuItem loss = operationsSubMenu.addItem("Списание", menuItemClickEvent -> lossModalWindow.open());
 
         //Производство
-
         MenuItem technologicalOperations = operationsSubMenu.addItem("Технологическая операция");
         technologicalOperations.addClickListener(e -> operations.getUI()
                 .ifPresent(ui -> ui.navigate("technologicalOperationsModal")));
