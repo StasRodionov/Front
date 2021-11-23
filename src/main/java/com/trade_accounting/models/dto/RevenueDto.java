ebce5086d439e76ccd6c55b31317dcda554daefd
Gwinhard <artmnvigor@gmail.com>
2021-11-23 23:21:50 +0700

    update RevenueDto

--- src/main/java/com/trade_accounting/models/dto/RevenueDto.java
+++ src/main/java/com/trade_accounting/models/dto/RevenueDto.java
@@ -35,4 +35,20 @@ public class RevenueDto {
 
     private BigDecimal amountShipment;      // InvoiceProduct
 
+    private Integer startOfPeriodAmount;
+
+    private Integer startOfPeriodSumOfPrice;
+
+    private Integer endOfPeriodAmount;
+
+    private Integer endOfPeriodSumOfPrice;
+
+    private Integer comingAmount;
+
+    private Integer comingSumOfPrice;
+
+    private Integer spendingAmount;
+
+    private Integer spendingSumOfPrice;
+
 }
