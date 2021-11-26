68e86827a201a7baf598260e11cfde425c0705ff
nickliq <nickliq@gmail.com>
2021-11-26 14:28:09 +0400

    front > Продажи > Прибыльность > реализовать фильтр.

--- src/main/java/com/trade_accounting/services/interfaces/api/InvoiceProductApi.java
+++ src/main/java/com/trade_accounting/services/interfaces/api/InvoiceProductApi.java
@@ -9,8 +9,10 @@ import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
+import retrofit2.http.QueryMap;
 
 import java.util.List;
+import java.util.Map;
 
 public interface InvoiceProductApi {
         @Headers("Accept: application/json")
@@ -36,5 +38,10 @@ public interface InvoiceProductApi {
         @Headers("Accept: application/json")
         @DELETE("{url}/{id}")
         Call<Void> deleteById(@Path(value = "url", encoded = true) String url, @Path("id") Long id);
+
+        @Headers("Accept: application/json")
+        @GET("{url}/search")
+        Call<List<InvoiceProductDto>> search(@Path(value = "url", encoded = true) String url,
+                                             @QueryMap Map<String, String> query);
 
 }
