94095fe2c63e2c45fc2928b6d244c9c1c10e0735
MaxIT95 <kuleshov995@yandex.ru>
2021-12-28 22:21:33 +0300

    Добавил фильтр в товары-обороты

--- src/main/java/com/trade_accounting/components/goods/RevenueView.java
+++ src/main/java/com/trade_accounting/components/goods/RevenueView.java
@@ -4,7 +4,6 @@ package com.trade_accounting.components.goods;
 import com.trade_accounting.components.AppView;
 import com.trade_accounting.components.util.GridFilter;
 import com.trade_accounting.components.util.GridPaginator;
-import com.trade_accounting.models.dto.InvoiceDto;
 import com.trade_accounting.models.dto.RevenueDto;
 import com.trade_accounting.services.interfaces.RevenueService;
 import com.vaadin.flow.component.UI;
@@ -28,6 +27,7 @@ import com.vaadin.flow.spring.annotation.UIScope;
 import org.springframework.context.annotation.Lazy;
 
 import java.util.List;
+import java.util.Map;
 
 @SpringComponent
 @PageTitle("Обороты")
@@ -45,14 +45,27 @@ public class RevenueView extends VerticalLayout {
 
     public RevenueView(RevenueService revenueService, @Lazy RevenueModalWindow revenueModalWindow) {
         this.revenueService = revenueService;
-        this.filter = new GridFilter<>(grid);
         this.data = getData();
         this.revenueModalWindow = revenueModalWindow;
         this.paginator = new GridPaginator<>(grid, data, 100);
         setSizeFull();
         configureGrid();
+        this.filter = new GridFilter<>(grid);
+        configureFilter();
         setHorizontalComponentAlignment(Alignment.CENTER, paginator);
         add(upperLayout(), filter, grid, paginator);
+    }
+
+
+    private void configureFilter() {
+        filter.setFieldToIntegerField("id");
+        filter.setFieldToComboBox("description", RevenueDto::getDescription, revenueService.getAll());
+        filter.setFieldToComboBox("unitShortName", RevenueDto::getUnitShortName, revenueService.getAll());
+        filter.onSearchClick(e -> {
+            Map<String, String> map = filter.getFilterData2();
+            paginator.setData(revenueService.search(map));
+        });
+        filter.onClearClick(e -> paginator.setData(getData()));
     }
 
     private void configureGrid() {
@@ -135,6 +148,7 @@ public class RevenueView extends VerticalLayout {
 
     private Button buttonFilter() {
         Button filterButton = new Button("Фильтр");
+        filterButton.addClickListener(e -> filter.setVisible(!filter.isVisible()));
         return filterButton;
     }
 
@@ -167,5 +181,4 @@ public class RevenueView extends VerticalLayout {
     private List<RevenueDto> getData() {
         return revenueService.getAll();
     }
-
 }
