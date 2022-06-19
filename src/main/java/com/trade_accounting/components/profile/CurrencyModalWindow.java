d1288e7ade3d7d21e20c63374ab062557496569c
Inna <kinvdev21@gmail.com>
2022-06-19 17:47:41 +0900

    Currency modal modified

--- src/main/java/com/trade_accounting/components/profile/CurrencyModalWindow.java
+++ src/main/java/com/trade_accounting/components/profile/CurrencyModalWindow.java
@@ -1,101 +1,105 @@
 package com.trade_accounting.components.profile;
 
 import com.trade_accounting.models.dto.units.CurrencyDto;
+import com.trade_accounting.models.dto.warehouse.ProductDto;
 import com.trade_accounting.services.interfaces.units.CurrencyService;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.HasSize;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.ErrorLevel;
+import com.vaadin.flow.data.value.ValueChangeMode;
+import com.vaadin.flow.spring.annotation.UIScope;
+import lombok.extern.slf4j.Slf4j;
 
+import static com.vaadin.flow.component.Tag.H2;
+
+@UIScope
+@Slf4j
 public class CurrencyModalWindow extends Dialog {
 
     private TextField shortNameField = new TextField();
-
     private TextArea fullNameField = new TextArea();
-
     private TextArea digitalCodeField = new TextArea();
-
     private TextArea letterCodeField = new TextArea();
-
     private TextArea sortNumberField = new TextArea();
-
     private Long id;
-
-    private final String labelWidth = "100px";
-
-    private final String fieldWidth = "400px";
-
     private final CurrencyService currencyService;
 
     public CurrencyModalWindow(CurrencyDto currencyDto,
                                CurrencyService currencyService) {
         this.currencyService = currencyService;
 
-        setCloseOnOutsideClick(false);
-        setCloseOnEsc(false);
+        setCloseOnOutsideClick(true);
+        setCloseOnEsc(true);
+
         id = currencyDto.getId();
         shortNameField.setValue(getFieldValueNotNull(currencyDto.getShortName()));
         fullNameField.setValue(getFieldValueNotNull(currencyDto.getFullName()));
         digitalCodeField.setValue(getFieldValueNotNull(currencyDto.getDigitalCode()));
         letterCodeField.setValue(getFieldValueNotNull(currencyDto.getLetterCode()));
         sortNumberField.setValue(getFieldValueNotNull(currencyDto.getSortNumber()));
-        add(new Text("Наименование"), header(),
-                new VerticalLayout(configureShortNameField(), configureDigitalCodeField(),
-                        configureLetterCodeField(), configureSortNumberField()
-                ));
+
+        fullNameField.setPlaceholder("Введите полное наименование");
+        fullNameField.setValueChangeMode(ValueChangeMode.EAGER);
+        shortNameField.setPlaceholder("Введите короткое наименование");
+        shortNameField.setValueChangeMode(ValueChangeMode.EAGER);
+        digitalCodeField.setPlaceholder("Введите цифровой код");
+        digitalCodeField.setValueChangeMode(ValueChangeMode.EAGER);
+        letterCodeField.setPlaceholder("Введите буквенный код");
+        letterCodeField.setValueChangeMode(ValueChangeMode.EAGER);
+        sortNumberField.setPlaceholder("Введите сортировочный номер");
+        sortNumberField.setValueChangeMode(ValueChangeMode.EAGER);
+
+        add(
+                header(),
+                getHorizontalLayout("Наименование", fullNameField),
+                getHorizontalLayout("Краткое наименование", shortNameField),
+                getHorizontalLayout("Цифровой код", digitalCodeField),
+                getHorizontalLayout("Буквенный код", letterCodeField),
+                getHorizontalLayout("Сортировочный номер", sortNumberField),
+                footer()
+        );
+
     }
 
-    private HorizontalLayout header() {
+    private Component header() {
         HorizontalLayout header = new HorizontalLayout();
-        fullNameField.setWidth("350px");
-        header.add(fullNameField, getSaveButton(), getCancelButton(), getDeleteButton());
+        H2 title = new H2("Валюта");
+        title.setWidth("345px");
+        header.add(title);
         return header;
     }
 
-    private HorizontalLayout configureShortNameField() {
+    private <T extends Component & HasSize> HorizontalLayout getHorizontalLayout(String labelText, T field) {
         HorizontalLayout horizontalLayout = new HorizontalLayout();
-        Label label = new Label("Краткое наименование");
-        label.setWidth(labelWidth);
-        horizontalLayout.add(label, shortNameField);
+        Label label = new Label(labelText);
+        field.setWidth("400px");
+        label.setWidth("200px");
+        horizontalLayout.setVerticalComponentAlignment(FlexComponent.Alignment.CENTER, label);
+        horizontalLayout.add(label, field);
         return horizontalLayout;
     }
 
-    private HorizontalLayout configureDigitalCodeField() {
-        HorizontalLayout horizontalLayout = new HorizontalLayout();
-        Label label = new Label("Цифровой код");
-        label.setWidth(labelWidth);
-        digitalCodeField.setWidth(fieldWidth);
-        digitalCodeField.getStyle().set("minHeight", "30px");
-        horizontalLayout.add(label, digitalCodeField);
-        return horizontalLayout;
-    }
-
-    private HorizontalLayout configureLetterCodeField() {
-        HorizontalLayout horizontalLayout = new HorizontalLayout();
-        Label label = new Label("Буквенный код");
-        label.setWidth(labelWidth);
-        letterCodeField.setWidth(fieldWidth);
-        letterCodeField.getStyle().set("minHeight", "60px");
-        horizontalLayout.add(label, letterCodeField);
-        return horizontalLayout;
-    }
-
-    private HorizontalLayout configureSortNumberField() {
-        HorizontalLayout horizontalLayout = new HorizontalLayout();
-        Label label = new Label("Сортировочный номер");
-        label.setWidth(labelWidth);
-        sortNumberField.setWidth(fieldWidth);
-        sortNumberField.getStyle().set("minHeight", "30px");
-        horizontalLayout.add(label, sortNumberField);
-        return horizontalLayout;
+    private HorizontalLayout footer() {
+        HorizontalLayout footer = new HorizontalLayout();
+        footer.add(getSaveButton(), getCancelButton(), getDeleteButton());
+        footer.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+        footer.setPadding(true);
+        return footer;
     }
 
     private Button getSaveButton() {
-        return new Button("Сохранить", event -> {
+        Button saveButton = new Button("Сохранить", event -> {
             CurrencyDto currencyDto = new CurrencyDto();
             currencyDto.setId(id);
             currencyDto.setShortName(shortNameField.getValue());
@@ -111,12 +115,15 @@ public class CurrencyModalWindow extends Dialog {
 
             close();
         });
+        saveButton.addThemeVariants(ButtonVariant.LUMO_SUCCESS);
+        return saveButton;
     }
 
     private Button getCancelButton() {
         Button cancelButton = new Button("Закрыть", event -> {
             close();
         });
+        cancelButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
         return cancelButton;
     }
 
@@ -125,6 +132,7 @@ public class CurrencyModalWindow extends Dialog {
             currencyService.deleteById(id);
             close();
         });
+        deleteButton.addThemeVariants(ButtonVariant.LUMO_ERROR);
         return deleteButton;
     }
 
